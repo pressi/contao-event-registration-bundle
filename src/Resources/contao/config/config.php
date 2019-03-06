d478d6699078517524d9ad2bfc89c7b131ae92ca
Stephan Preßl <mail@stephanpressl.at>
2019-03-06 15:33:16 +0100

    add first table and add contao config file

---
+++ src/Resources/contao/config/config.php
@@ -0,0 +1,18 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2019 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by PreStep
+ * www.prestep.at <development@prestep.at>
+ *
+ *******************************************************************/
+
+
+/**
+ * Backend modules
+ */
+
+$GLOBALS['BE_MOD']['content']['calendar']['tables'][] = 'tl_prestep_calendar_events_registration';
---
+++ src/Resources/contao/dca/tl_prestep_calendar_events_registration.php
@@ -0,0 +1,11 @@
+<?php
+/*******************************************************************
+ *
+ * (c) 2019 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ *
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by PreStep
+ * www.prestep.at <development@prestep.at>
+ *
+ *******************************************************************/
